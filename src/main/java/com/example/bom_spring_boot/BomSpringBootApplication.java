660a3d1013f8352d0e40617986442da41a3a43c9
noppohnsupap <noppohn.sup@ascendcorp.com>
2021-09-01 13:30:32 +0700

    first time

---
+++ src/main/java/com/example/bom_spring_boot/BomSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.bom_spring_boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BomSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BomSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/bom_spring_boot/BomSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bom_spring_boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BomSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
